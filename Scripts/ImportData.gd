58ef34f9bc29ac8c2dd4dd42f724e0c5eb369998
Daniel Boxer <dani3lb0x3r@gmail.com>
2022-03-05 13:13:19 -0500

    Refactored ImportData script

--- Scripts/ImportData.gd
+++ Scripts/ImportData.gd
@@ -4,20 +4,21 @@ var keypoint_data: Dictionary setget , get_keypoint_data
 
 
 func _ready() -> void:
-	var keys := ["left_hand_data", "right_hand_data"]
-	var files := {
-		keys[0]: "res://Data/hand_keypoints_left.json",
-		keys[1]: "res://Data/hand_keypoints_right.json"
-	}
-	for file_number in range(2):
-		# get keypoint data from file
-		var data_file := File.new()
-		var _error_code: int = data_file.open(files[keys[file_number]], File.READ)
-		var data_json: JSONParseResult = JSON.parse(data_file.get_as_text())
-		data_file.close()
-		var dict_data: Dictionary = data_json.result
-		keypoint_data[keys[file_number]] = dict_data["hand_array"]
+	var file_1 := "res://Data/hand_keypoints_left.json"
+	var file_2 := "res://Data/hand_keypoints_right.json"
+	keypoint_data["left_hand_data"] = import_data(file_1)["hand_array"]
+	keypoint_data["right_hand_data"] = import_data(file_2)["hand_array"]
 
 
 func get_keypoint_data() -> Dictionary:
 	return keypoint_data
+
+
+func import_data(file_name: String) -> Dictionary:
+	# get keypoint data from file
+	var data_file := File.new()
+	var _error_code: int = data_file.open(file_name, File.READ)
+	var data_json: JSONParseResult = JSON.parse(data_file.get_as_text())
+	data_file.close()
+	var dict_data: Dictionary = data_json.result
+	return dict_data
