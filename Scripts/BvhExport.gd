cfbbdddb9a14eb6aa536eb495847d984c3d68520
Daniel Boxer <dani3lb0x3r@gmail.com>
2022-05-11 17:44:19 -0400

    Added recording button

--- Scripts/BvhExport.gd
+++ Scripts/BvhExport.gd
@@ -11,23 +11,26 @@ onready var progress_bar: ProgressBar = get_node(
 var recording_end_frame := 0
 var progress_bar_increase := 1.0
 var open_file_name := ""
+var frame_count := 1
 
 
 func _ready():
 	self.visible = false
-	set_physics_process(false)
+	self.set_physics_process(false)
 
 
 func _physics_process(_delta):
-	if ImportData.frame_number == recording_end_frame:
+	if ImportData.frame_number >= recording_end_frame:
 		progress_bar.value = 0
-		stop_recording()
+		self.stop_recording()
 	else:
 		progress_bar.value += progress_bar_increase
 
 
 # Produces the skeleton hierarchy that appears at the start of a .bvh file.
-func generate_hierarchy(file_name: String, skeleton) -> void:
+func generate_hierarchy(
+	file_name: String, skeleton: Skeleton, is_recording_end_set := true
+) -> void:
 	var file = File.new()
 	file.open(file_name, File.WRITE)
 	file.store_line("HIERARCHY")
@@ -72,12 +75,19 @@ func generate_hierarchy(file_name: String, skeleton) -> void:
 
 	file.store_line("}")
 	file.store_line("MOTION")
-	# +1 because frames are 0 indexed
-	# +1 for the first frame which is all 0.00
-	file.store_line(
-		"Frames: " + str(ExportSettings.end_frame - ExportSettings.start_frame + 2)
-	)
-	file.store_line("Frame Time: " + str(1.0 / Engine.iterations_per_second))
+	if is_recording_end_set:
+		# +1 for the first frame which is all 0.00
+		file.store_line(
+			"Frames: " + str(ExportSettings.end_frame - ExportSettings.start_frame + 1)
+		)
+	else:
+		# this line is filled with spaces because the frame count is currently unknown
+		# when the frame count is added, it will overwrite this line
+		# the spaces prevent it from overwriting into the next line
+		# there will be some spaces left over, but that doesn't interfere with the format
+		file.store_line("                                                               ")
+	file.store_line("Frame Time: " + keep_point(str(1.0 / Engine.iterations_per_second)))
+
 	# the first frame has no rotation
 	add_empty_frame(file, skeleton)
 	file.close()
@@ -106,17 +116,25 @@ func end_site(file: File, depth: int) -> int:
 
 
 # Adds empty frame to input file.
-func add_empty_frame(file: File, skeleton) -> void:
+func add_empty_frame(file: File, skeleton: Skeleton) -> void:
 	var temp := ""
 	for _i in range(skeleton.get_bone_count() * 3 + 3):
 		temp += "0 "
-	file.store_line(temp)
+	file.store_line(temp.trim_suffix(" "))
+
+
+# Adds a decimal point if the input doesn't have one.
+func keep_point(num: String) -> String:
+	if not "." in num:
+		num += ".0"
+	return num
 
 
 # Adds frame of data to current recording.
 func add_recording_frame(frame: Dictionary) -> void:
-	var file = File.new()
-	file.open(open_file_name, File.READ_WRITE)
+	frame_count += 1
+	var file := File.new()
+	var _error_code: int = file.open(open_file_name, File.READ_WRITE)
 	file.seek_end()
 
 	var line := ""
@@ -133,7 +151,7 @@ func add_recording_frame(frame: Dictionary) -> void:
 		line += str(rad2deg(frame[bone].y)) + " "
 		line += str(rad2deg(frame[bone].z)) + " "
 
-	file.store_line(line)
+	file.store_line(line.trim_suffix(" "))
 
 	file.close()
 
@@ -152,29 +170,56 @@ func calculate_offset(bone_child: int, bone_parent: int, skeleton) -> Vector3:
 	return bone_offset
 
 
-# Starts recording.
+# Starts recording. If the `Generate BVH` button was pressed, a progress bar will show.
 func start_recording(
-	filename: String, skeleton: Skeleton, start_frame: int, end_frame: int
+	filename: String, skeleton: Skeleton, start_frame := -1, end_frame := -1
 ) -> void:
 	open_file_name = filename
-	generate_hierarchy(open_file_name, skeleton)
-	Engine.iterations_per_second = 1500
-	ImportData.set_frame_number(start_frame - 1)
-	recording_end_frame = end_frame - 1
-	progress_bar_increase = 100.0 / (end_frame - start_frame + 1)
+	frame_count = 1
+
+	if start_frame != -1 || end_frame != -1:
+		# if "Generate BVH" button is pressed
+		generate_hierarchy(open_file_name, skeleton)
+		Engine.iterations_per_second = 1500
+		ImportData.set_frame_number(start_frame - 1)
+		recording_end_frame = end_frame - 1
+		progress_bar_increase = 100.0 / (end_frame - start_frame + 1)
+		self.set_physics_process(true)
+		self.visible = true
+		Pause.toggle_pause()
+		# if pause is set to visible before it's toggled, it won't work
+		Pause.visible = true
+		Pause.hide_overlays()
+	else:
+		generate_hierarchy(open_file_name, skeleton, false)
+
+	# this tells the Hand script to start recording
 	for bone in range(skeleton.get_bone_count()):
 		Hand.recording_frame[bone] = Vector3.ZERO
-	set_physics_process(true)
-	Pause.toggle_pause()
-	Pause.visible = true
-	Pause.hide_overlays()
 
 
 # Adds recorded data to file and stops recording.
-func stop_recording() -> void:
-	get_node("/root/Main/Pause/MenuOverlay/MenuContainer/Settings").emit_signal("pressed")
+func stop_recording(is_recording_end_set := true) -> void:
+	if is_recording_end_set:
+		# if "Generate BVH" button was pressed
+		Pause.toggle_pause()
+		Pause.activate_popup("Recording finished")
+		var settings := get_node("/root/Main/Pause/MenuOverlay/MenuContainer/Settings")
+		settings.emit_signal("pressed")
+		self.visible = false
+		Engine.iterations_per_second = 1
+		self.set_physics_process(false)
+	else:
+		# if recording button was pressed
+		# the frame count can now be added to the file
+		var file := File.new()
+		var _error_code: int = file.open(open_file_name, File.READ_WRITE)
+		# go to line 153
+		for i in 152:
+			var _line := file.get_line()
+		# use `store_string` instead of `store_line` because there is already a new line
+		file.store_string("Frames: " + str(frame_count))
+		file.close()
+
+	# clearing this variable tells the Hand script to stop recording
 	Hand.recording_frame.clear()
-	self.visible = false
-	Engine.iterations_per_second = 1
-	Pause.toggle_pause()
-	set_physics_process(false)
--- Scripts/ExportSettings.gd
+++ Scripts/ExportSettings.gd
@@ -4,30 +4,34 @@ extends Control
 enum hands { LEFT, RIGHT }
 
 export var bvh_button_path: NodePath
+export var recording_button_path: NodePath
 export var frame_start_label_path: NodePath
 export var frame_end_label_path: NodePath
 export var frame_start_slider_path: NodePath
 export var frame_end_slider_path: NodePath
 export var frame_start_input_path: NodePath
 export var frame_end_input_path: NodePath
-export var bvh_overlay_path: NodePath
 export var hand_bvh_option_button_path: NodePath
 
 var start_frame := 0 setget , get_start_frame
-var end_frame := 0 setget , get_end_frame
-var hand_recording = hands.LEFT
+var end_frame := 1 setget , get_end_frame
+var hand_recording: int = hands.LEFT
 
 onready var bvh_button: Button = get_node(bvh_button_path)
+onready var recording_button: Button = get_node(recording_button_path)
 onready var frame_start_label: Label = get_node(frame_start_label_path)
 onready var frame_end_label: Label = get_node(frame_end_label_path)
 onready var frame_start_slider: HSlider = get_node(frame_start_slider_path)
 onready var frame_end_slider: HSlider = get_node(frame_end_slider_path)
 onready var frame_start_input: SpinBox = get_node(frame_start_input_path)
 onready var frame_end_input: SpinBox = get_node(frame_end_input_path)
-onready var bvh_overlay: ColorRect = get_node(bvh_overlay_path)
-onready var hand_bvh_option_button = get_node(hand_bvh_option_button_path)
-onready var left_skeleton := get_node("/root/Main/Hands/LeftHand/Left_Hand/Skeleton")
-onready var right_skeleton := get_node("/root/Main/Hands/RightHand/Right_Hand/Skeleton")
+onready var hand_bvh_option_button: OptionButton = get_node(hand_bvh_option_button_path)
+onready var left_skeleton: Skeleton = get_node(
+	"/root/Main/Hands/LeftHand/Left_Hand/Skeleton"
+)
+onready var right_skeleton: Skeleton = get_node(
+	"/root/Main/Hands/RightHand/Right_Hand/Skeleton"
+)
 
 onready var ViewSettings := get_node("/root/Main/Pause/SettingsOverlay/Settings/View")
 onready var Pause := get_node("/root/Main/Pause")
@@ -57,7 +61,7 @@ func get_end_frame() -> int:
 
 # Returns the current date and time for a file name.
 func format_datetime() -> String:
-	var formatted: String = (
+	return (
 		str(OS.get_datetime().year)
 		+ "_"
 		+ str(OS.get_datetime().month)
@@ -70,60 +74,81 @@ func format_datetime() -> String:
 		+ "_"
 		+ str(OS.get_datetime().second)
 	)
-	return formatted
 
 
-# Creates file and starts recording.
-func _on_BVH_pressed() -> void:
-	bvh_overlay.visible = true
+# Returns the skeleton of the hand being recorded.
+func get_skeleton() -> Skeleton:
+	if hand_recording == hands.LEFT:
+		return left_skeleton
+	return right_skeleton
+
+
+# Returns a unique file name.
+func new_file_name() -> String:
 	var file_name: String = format_datetime()  # date time format avoids overwriting files
 	var file_location := "res://Output/"
 	var file_type := ".bvh"
 	var file_hand: String
-	var skeleton: Skeleton
 	if hand_recording == hands.LEFT:
 		ViewSettings.set_hand_visibility(hands.LEFT, true)
 		ViewSettings.set_hand_visibility(hands.RIGHT, false)
 		file_hand = "L_"
-		skeleton = left_skeleton
 	else:
 		ViewSettings.set_hand_visibility(hands.LEFT, false)
 		ViewSettings.set_hand_visibility(hands.RIGHT, true)
 		file_hand = "R_"
-		skeleton = right_skeleton
 	var open_file_name = file_location + file_hand + file_name + file_type
-	BvhExport.start_recording(open_file_name, skeleton, start_frame, end_frame)
+	return open_file_name
+
+
+# Starts recording. The recording will be stopped when the end frame is reached.
+func _on_BVH_pressed() -> void:
+	BvhExport.start_recording(new_file_name(), get_skeleton(), start_frame, end_frame)
+
+
+# Starts recording. The recording will be stopped when the button is pressed again.
+func _on_RecordingButton_pressed() -> void:
+	if recording_button.get_text() == "Start Recording":
+		Pause.activate_popup("Recording started")
+		BvhExport.start_recording(new_file_name(), get_skeleton())
+		recording_button.set_text("Stop Recording")
+	else:
+		Pause.activate_popup("Recording finished")
+		BvhExport.stop_recording(false)
+		recording_button.set_text("Start Recording")
 
 
 # Shows information.
 func _on_BVHInfo_pressed() -> void:
 	Pause.activate_popup(
 		(
-			"Generate a .bvh file to the Output folder\n"
-			+ "This file will contain armature and animation data\n"
-			+ "The .bvh file can be imported to other software"
+			"Press 'Generate BVH' to create a .bvh file (Frame Start to Frame End).\n"
+			+ "Press 'Start Recording' to start recording the file at the current frame."
+			+ " Pressing the button again will stop the recording.\n"
+			+ "The .bvh files (located in the Output folder) contain armature and "
+			+ "animation data and can be imported to other software"
 		)
 	)
 
 
 # Sets frame start to input value.
 func _on_FrameStartSlider_value_changed(value: int) -> void:
-	if value <= end_frame:
+	if value < end_frame:
 		frame_start_label.text = "Frame Start: " + str(value)
 		frame_start_input.value = value
 		start_frame = value
 	else:
-		Pause.activate_popup("Start frame can't be greater than end frame")
+		Pause.activate_popup("Start frame can't be greater than or equal to end frame")
 
 
 # Sets frame start to input value.
 func _on_FrameStartInput_value_changed(value: int) -> void:
-	if value <= end_frame:
+	if value < end_frame:
 		frame_start_label.text = "Frame Start: " + str(value)
 		frame_start_slider.value = value
 		start_frame = value
 	else:
-		Pause.activate_popup("Start frame can't be less than end frame")
+		Pause.activate_popup("Start frame can't be less than or equal to end frame")
 
 
 # Shows information.
